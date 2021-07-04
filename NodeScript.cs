270fa1040bc66a4c7165412f0d24251281f67465
qkrwodsbfjq <52436915+qkrwodsbfjq@users.noreply.github.com>
2021-07-04 09:19:43 +0900

    Add files via upload

---
+++ NodeScript.cs
@@ -0,0 +1,229 @@
+﻿//==========================================
+// writer : 노현석.
+// file : NodeScript.cs.
+// content : 월드맵에서 플레이어가 이동할 수 있는 노드 컨트롤러.
+// discript : [SerializeField]처리된 상하좌우 노드 위치에 따른 다른 노드를 배치해 플레이어가 그간 이동하도록 제어.
+//==========================================
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NodeScript : MonoBehaviour
+{
+    [SerializeField] GameObject leftNode; //1
+    [SerializeField] GameObject rightNode; //2
+    [SerializeField] GameObject upNode; //3
+    [SerializeField] GameObject downNode; //4
+    [SerializeField] GameObject WorldMapPlayer;
+
+    [SerializeField] string sceneName;
+    [SerializeField] bool isPassable = true; //노드로 갈 수 있는지 여부, 아직 작업중.
+
+    [SerializeField] public int[] nodeStageDebuffID;
+
+    PlayerStatus myPlayerStatus;
+
+    void Awake()
+    {
+        myPlayerStatus = FindObjectOfType<PlayerStatus>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    //==========================================
+
+    void Update()
+    {
+        
+    }
+
+    //==========================================
+
+    public bool CheckIfNodeIsEmpty(float nodeNumber)
+    {
+        if(nodeNumber == 1) //left
+        {
+            if(leftNode.gameObject == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else if(nodeNumber == 2) //right
+        {
+            if (rightNode.gameObject == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else if (nodeNumber == 3) //up
+        {
+            if (upNode.gameObject == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else if (nodeNumber == 4) //down
+        {
+            if (downNode.gameObject == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    //==========================================
+
+    public Vector2 ReturnTargetNodeLocation(float nodeNumber)
+    {
+        if (nodeNumber == 1)
+        {
+            return leftNode.transform.position;
+        }
+        else if(nodeNumber == 2)
+        {
+            return rightNode.transform.position;
+        }
+        else if(nodeNumber == 3)
+        {
+            return upNode.transform.position;
+        }
+        else if(nodeNumber == 4)
+        {
+            return downNode.transform.position;
+        }
+        else
+        {
+            return gameObject.transform.position;
+        }
+    }
+
+    //==========================================
+
+    public GameObject ReturnTargetNode(float nodeNumber)
+    {
+        if (nodeNumber == 1)
+        {
+            return leftNode;
+        }
+        else if (nodeNumber == 2)
+        {
+            return rightNode;
+        }
+        else if (nodeNumber == 3)
+        {
+            return upNode;
+        }
+        else if (nodeNumber == 4)
+        {
+            return downNode;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    //==========================================
+
+    public void MoveToScene()
+    {
+        if(String.IsNullOrEmpty(sceneName))
+        {
+
+        }
+        else
+        {
+            FindObjectOfType<SceneController>().MoveToScene(sceneName);
+        }
+    }
+
+    //==========================================
+
+    public string ReturnSceneName()
+    {
+        return sceneName;
+    }
+
+
+    //==========================================
+
+    public bool isNodePassable()
+    {
+        return isPassable;
+    }
+
+    //==========================================
+
+    public bool IsTargetNodePassable(float nodeNumber)
+    {
+        switch (nodeNumber)
+        {
+            case 1:
+                try
+                {
+                    return leftNode.GetComponent<NodeScript>().isNodePassable();
+                }
+                catch
+                {
+                    return false;
+                }
+            case 2:
+                try
+                {
+                    return rightNode.GetComponent<NodeScript>().isNodePassable();
+                }
+                catch
+                {
+                    return false;
+                }
+            case 3:
+                try
+                {
+                    return upNode.GetComponent<NodeScript>().isNodePassable();
+                }
+                catch
+                {
+                    return false;
+                }
+            case 4:
+                try
+                {
+                    return downNode.GetComponent<NodeScript>().isNodePassable();
+                }
+                catch
+                {
+                    return false;
+                }
+            default:
+                return false;
+        }
+    }
+}
---
+++ WorldMapPlayerScript.cs
@@ -0,0 +1,184 @@
+﻿//==========================================
+// writer : 노현석.
+// file : WorldMapPlayerScript.cs.
+// content : 월드맵에서 플레이어 제어 컨트롤러.
+// discript : 게임 오브젝트가 항상 currentNode의 노드에 위치함.
+//==========================================
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class WorldMapPlayerScript : MonoBehaviour
+{
+    [SerializeField] GameObject currentNode;
+    [SerializeField] float minimumDistanceForPlayerToMove = 1f;
+    [SerializeField] float moveSpeed;
+
+    bool playerCanInput = true;
+    //PlayerStatus myPlayerStatus;
+    StatusHandler myPlayerStatus;
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        transform.position = currentNode.transform.position;
+    }
+
+    //==========================================
+
+    void Update()
+    {
+        if (playerCanInput)
+        {
+            //myPlayerStatus = FindObjectOfType<PlayerStatus>();
+            myPlayerStatus = FindObjectOfType<StatusHandler>();
+            MovePlayer();
+            myPlayerStatus.UpdatePlayerStatusOnWorldMap(currentNode);
+        }
+        MoveCharacterToTargetNode();
+        MoveScene();
+    }
+
+    //==========================================
+
+    private void MovePlayer()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (currentNode.GetComponent<NodeScript>().IsTargetNodePassable(1))
+            {
+                MoveLeft();
+            }
+            else
+            {
+
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (currentNode.GetComponent<NodeScript>().IsTargetNodePassable(2))
+            {
+                MoveRight();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (currentNode.GetComponent<NodeScript>().IsTargetNodePassable(3))
+            {
+                MoveUp();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (currentNode.GetComponent<NodeScript>().IsTargetNodePassable(4))
+            {
+                MoveDown();
+            }
+        }
+        else
+        {
+
+        }
+    }
+
+    //==========================================
+
+    private void MoveLeft()
+    {
+        if (currentNode.GetComponent<NodeScript>().CheckIfNodeIsEmpty(1))
+        {
+            currentNode = currentNode.GetComponent<NodeScript>().ReturnTargetNode(1); 
+            myPlayerStatus.currentNode = this.currentNode;
+        }
+        else
+        {
+
+        }
+    }
+
+    //==========================================
+
+    private void MoveRight()
+    {
+        if(currentNode.GetComponent<NodeScript>().CheckIfNodeIsEmpty(2))
+        {
+            currentNode = currentNode.GetComponent<NodeScript>().ReturnTargetNode(2); 
+            myPlayerStatus.currentNode = this.currentNode;
+        }
+        else
+        {
+
+        }
+    }
+
+    //==========================================
+
+    private void MoveUp()
+    {
+        if (currentNode.GetComponent<NodeScript>().CheckIfNodeIsEmpty(3))
+        {
+            currentNode = currentNode.GetComponent<NodeScript>().ReturnTargetNode(3); 
+            myPlayerStatus.currentNode = this.currentNode;
+        }
+        else
+        {
+
+        }
+    }
+
+    //==========================================
+
+    private void MoveDown()
+    {
+        if (currentNode.GetComponent<NodeScript>().CheckIfNodeIsEmpty(4))
+        {
+            currentNode = currentNode.GetComponent<NodeScript>().ReturnTargetNode(4);
+            myPlayerStatus.currentNode = this.currentNode;
+        }
+        else
+        {
+
+        }
+    }
+
+    //==========================================
+
+    public void MoveCharacterToTargetNode()
+    {
+        if(transform.position != currentNode.transform.position)
+        {
+            playerCanInput = false;
+            transform.position = Vector2.MoveTowards(transform.position, currentNode.transform.position, moveSpeed);
+            if(Vector2.Distance(transform.position, currentNode.transform.position) < minimumDistanceForPlayerToMove)
+            {
+                playerCanInput = true;
+            }
+        }
+        else
+        {
+
+        }
+    }
+
+    //==========================================
+
+    public void MoveScene()
+    {
+        if (Input.GetButtonDown("Submit"))
+        {
+            if (currentNode.GetComponent<NodeScript>().ReturnSceneName() == null)
+            {
+
+            }
+            else
+            {
+                currentNode.GetComponent<NodeScript>().MoveToScene();
+            }
+        }
+    }
+}
