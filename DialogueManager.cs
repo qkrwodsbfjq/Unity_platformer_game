84234422bbba3e67c754d611031408b1569af7d0
zzaeuni <52436915+qkrwodsbfjq@users.noreply.github.com>
2021-07-10 21:53:51 +0900

    Add files via upload

---
+++ DialogueManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] GameObject go_Dialogue;
+    [SerializeField] Text txt_Dialogue;
+    [SerializeField] Text txt_Name;
+
+    Dialogue[] dialogues;
+
+    bool isDialogue = false; // 대화중일 경우 true.
+    bool isNext = false; //space 입력 대기.
+
+    int lineCount = 0;
+    int contextCount = 0;
+
+    [Header("텍스트 출력 딜레이")]
+    [SerializeField] float textDelay;
+
+    void Update()
+    {
+        if (isDialogue)
+        {
+            if (isNext)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    isNext = false;
+                    txt_Dialogue.text = "";
+                    if (++contextCount < dialogues[lineCount].contexts.Length)
+                    {
+                        StartCoroutine(TypeWriter());
+                    }
+                    else
+                    {
+                        contextCount = 0;
+                        if(++lineCount < dialogues.Length)
+                        {
+                            StartCoroutine(TypeWriter());
+                        }
+                        else
+                        {
+                            EndDialogue();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void ShowDialogue(Dialogue[] p_dialogues)
+    {
+        isDialogue = true;
+        txt_Dialogue.text = "";
+        txt_Name.text = "";
+
+        dialogues = p_dialogues;
+
+        StartCoroutine(TypeWriter());
+    }
+    void EndDialogue()
+    {
+        isDialogue = false;
+        contextCount = 0;
+        lineCount = 0;
+        dialogues = null;
+        isNext = false;
+        SettingUI(false);
+    }
+
+    IEnumerator TypeWriter()
+    {
+        SettingUI(true);
+
+        string t_ReplaceText = dialogues[lineCount].contexts[contextCount];
+        t_ReplaceText = t_ReplaceText.Replace("'", ","); // '를 ,로 치환
+        t_ReplaceText = t_ReplaceText.Replace("\\n", "\n");
+
+        txt_Name.text = dialogues[lineCount].name;
+
+        for(int i = 0; i< t_ReplaceText.Length; i++)
+        {
+            txt_Dialogue.text += t_ReplaceText[i];
+            yield return new WaitForSeconds(textDelay);
+        }
+
+        isNext = true;
+    }
+
+    void SettingUI(bool p_flag)
+    {
+        go_Dialogue.SetActive(p_flag);
+    }
+}
