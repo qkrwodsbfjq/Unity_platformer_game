9d6229ad7bc0c67bb6e6b6020f35aacad313f45d
zzaeuni <52436915+qkrwodsbfjq@users.noreply.github.com>
2021-11-28 02:54:42 +0900

    Add files via upload

--- ex04.cpp
+++ ex04.cpp
@@ -1,32 +1,69 @@
+// Implementation file for StackType.h
+
+//A. 맨 마지막에 들어온 원소
+//B. 구현
+//C. heap을 사용한 우선순위 큐를 사용한 경우 Push는 원래 O(1)이었지만 O(logN)이 됨, Pop은 원래 O(1)이었지만 O(logN)이 됨.
+
+#include <cstddef>
+#include <new>
 #include <iostream>
-#define MAX_ROWS 10
-#define MAX_COLS 10
 
 using namespace std;
+#include "StackType.h"
+void StackType::Push(ItemType newItem)
+// Adds newItem to the top of the stack.
+// Pre:  Stack has been initialized.
+// Post: If stack is full, FullStack exception is thrown;
+//       else newItem is at the top of the stack.
 
-int mat[MAX_ROWS][MAX_COLS];
-int NumPaths(int row, int col, int n);
-
-int main() {
-	for (int i = 0; i < MAX_ROWS; i++) {
-		for (int j = 0; j < MAX_COLS; j++) {
-			mat[i][j] = -1;
-		}
-	}
-
-	cout << NumPaths(1, 1, 5);
-
-	return 0;
-}
-
-int NumPaths(int row, int col, int n) {
-	if (mat[row][col] != -1)
-		return mat[row][col];
-
-	if ((row == n) || (col == n))
-		mat[row][col] = 1;
+{
+	if (IsFull())
+		throw FullStack();
 	else
-		mat[row][col] = NumPaths(row + 1, col, n) + NumPaths(row, col + 1, n);
-	
-	return mat[row][col];
+	{
+		StackItem item;
+		item.timeStamp = timeStamp++;
+		item.value = newItem;
+		pq.Enqueue(item);
+	}
 }
+ItemType StackType::Pop()
+// Removes top item from Stack and returns it in item.
+// Pre:  Stack has been initialized.
+// Post: If stack is empty, EmptyStack exception is thrown;
+//       else top element has been removed.
+{
+	if (IsEmpty())
+		throw EmptyStack();
+	else
+	{
+		StackItem item;
+		pq.Dequeue(item);
+		return item.value;
+	}
+}
+bool StackType::IsEmpty() const
+// Returns true if there are no elements on the stack; false otherwise.
+{
+	return pq.IsEmpty();
+}
+bool StackType::IsFull() const
+// Returns true if there is no room for another ItemType 
+//  on the free store; false otherwise.
+{
+	return pq.IsFull();
+}
+
+StackType::~StackType()
+// Post: stack is empty; all items have been deallocated.
+{
+
+}
+
+StackType::StackType()	// Class constructor.
+	:pq(50)
+{
+	
+	timeStamp = 0;
+}
+
